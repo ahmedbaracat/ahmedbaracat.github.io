3a512cb02f5aabb6175aea582d8fdab88877ff62
Baracat <baracat@aucegypt.edu>
2020-06-06 22:15:52 +0100

    Add a hack to allow navigating to other pages when url has hash

--- assets/smooth_scrolling.js
+++ assets/smooth_scrolling.js
@@ -1,24 +1,25 @@
 $(document).ready(function(){
 // Add smooth scrolling to all links
 $("a").on('click', function(event) {
-
   // Make sure this.hash has a value before overriding default behavior
   if (this.hash !== "") {
-    // Prevent default anchor click behavior
-    event.preventDefault();
-
     // Store hash
     var hash = this.hash;
 
-    // Using jQuery's animate() method to add smooth page scroll
-    // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-    $('html, body').animate({
-      scrollTop: $(hash).offset().top
-    }, 800, function(){
+    //hack used to allow navigating to other pages when url has hash
+    if($(hash).offset() !== undefined) {
+        // Prevent default anchor click behavior
+        event.preventDefault();
+        // Using jQuery's animate() method to add smooth page scroll
+        // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+        $('html, body').animate({
+          scrollTop: $(hash).offset().top
+        }, 800, function(){
 
-      // Add hash (#) to URL when done scrolling (default click behavior)
-      window.location.hash = hash;
-    });
+          // Add hash (#) to URL when done scrolling (default click behavior)
+          window.location.hash = hash;
+        });
+    }
   } // End if
 });
 });
